c586267643d2243d772335f3c9c866a8c267f96e
Sal Frisina+++++++++++++ <Sfrisina@gmail.com>
2022-09-14 21:15:31 -0400

    updating item model

---
+++ controllers/afterLogin.js
@@ -0,0 +1,26 @@
+const Item = require('../models/Item')
+
+module.exports = {
+    getTodos: async (req, res) => {
+        try {
+            res.render('afterLogin.ejs')
+        }catch(err) {
+            console.log(err)
+        }
+    }, 
+    createItem: async (req, res) => {
+        try {
+            await Item.create({
+                itemNum: req.body.itemNum,
+                description: req.body.description,
+                quantity: req.body.quantity, 
+                image: req.body.image,
+                price: req.body.price, 
+                inCart: false, })
+                console.log('Item Added')
+                res.redirect('/afterLogin')
+        }catch (err){
+            console.log(err);
+        }
+    }
+}
