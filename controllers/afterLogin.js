8b0e08647879abc640e4dd420da7fbf7f2c007bf
Sal Frisina <sfrisina@gmail.com>
2022-09-19 21:39:30 -0400

    "uppload"

--- controllers/afterLogin.js
+++ controllers/afterLogin.js
@@ -14,12 +14,8 @@ module.exports = {
                 itemNum: req.body.itemNum,
                 description: req.body.description,
                 quantity: req.body.quantity, 
-<<<<<<< HEAD
                 image: req.body.image,
                 price: req.body.price, 
-=======
-                image: req.body.image, 
->>>>>>> b4cc2f24589441322feee41aa6e5551d1004dde4
                 inCart: false, })
                 console.log('Item Added')
                 res.redirect('/afterLogin')
